cac8a7798323c81fbbbb87f3db1d9bc8775200fd
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-08-31 15:51:26 +0530

    create ComplaintResponseDto

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/dto/impl/ComplaintResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.HostelManagementSystem_Backend.dto.impl;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ComplaintResponseDto {
+
+    private String complaintId;
+    private String studentName;
+    private String roomId;
+    private String description;
+    private LocalDate date;
+    private String priority;
+}
