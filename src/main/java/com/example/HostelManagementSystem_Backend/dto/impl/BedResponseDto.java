f8dfb5c6edd7d18af3c0672b0a6c7d59de866b2a
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-08-31 15:49:15 +0530

    create BedResponseDto

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/dto/impl/BedResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.HostelManagementSystem_Backend.dto.impl;
+
+import com.example.HostelManagementSystem_Backend.dto.SuperDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BedResponseDto implements SuperDto {
+
+    private String bedId;
+    private String type;
+    private String description;
+    private Boolean availability;
+    private String roomId;
+    private int roomNo;
+}
