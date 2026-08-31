0808bf101dd22c01c282a1ca90e75a8d906c425f
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-08-31 15:49:36 +0530

    create ComplaintCreateDto

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/dto/impl/ComplaintCreateDto.java
@@ -0,0 +1,13 @@
+package com.example.HostelManagementSystem_Backend.dto.impl;
+
+import com.example.HostelManagementSystem_Backend.dto.SuperDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ComplaintCreateDto implements SuperDto {
+    private String description;
+}
