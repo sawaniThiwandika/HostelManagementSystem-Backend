ab1be722ea0b5cd697ffa4eaa4ab2c3aa8a85a88
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-02 08:36:47 +0530

    update parent entity attributes- add NIC field

--- src/main/java/com/example/HostelManagementSystem_Backend/entity/impl/ParentEntity.java
+++ src/main/java/com/example/HostelManagementSystem_Backend/entity/impl/ParentEntity.java
@@ -1,31 +1,37 @@
 package com.example.HostelManagementSystem_Backend.entity.impl;
 
 import com.example.HostelManagementSystem_Backend.entity.SuperEntity;
-import com.example.HostelManagementSystem_Backend.entity.impl.StudentEntity;
+
+
 import jakarta.persistence.*;
 import lombok.Data;
-import java.util.List;
 
+import java.time.LocalDate;
+import java.util.List;
+@Data
 @Entity
 @Table(name = "parent")
-@Data
 public class ParentEntity implements SuperEntity {
+
     @Id
     @Column(name = "parent_id", length = 10)
     private String parentId;
 
+    @Column(name = "parent_NIC", length = 12, unique = true, nullable = false)
+    private String parentNIC;
+
     @Column(name = "parent_name", nullable = false, length = 100)
     private String parentName;
 
-    @Column(name = "parent_tel", length = 15)
+    @Column(name = "parent_tel", nullable = false, length = 15)
     private String parentTel;
 
-    @Column(name = "parent_address", length = 255)
+    @Column(name = "parent_address", nullable = false, length = 255)
     private String parentAddress;
 
     @Column(name = "parent_email", length = 100)
     private String parentEmail;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent")
     private List<StudentEntity> students;
 }
