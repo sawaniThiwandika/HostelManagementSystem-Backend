9500a96ba3d6a2dbc865687c1471946f54a26b2f
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-02 08:43:52 +0530

    create StudentSummaryDto class

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/dto/impl/StudentSummaryDto.java
@@ -0,0 +1,19 @@
+package com.example.HostelManagementSystem_Backend.dto.impl;
+
+
+import com.example.HostelManagementSystem_Backend.dto.SuperDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentSummaryDto implements SuperDto {
+
+    private String studentId;
+    private String name;
+    private String tel;
+    private String jobEducationInstitute;
+    private String positionFaculty;
+}
