cb09644999df898f2f882309b68ef8bc7a74feef
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-02 08:43:39 +0530

    create StudentResponseDto class

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/dto/impl/StudentResponseDto.java
@@ -0,0 +1,30 @@
+package com.example.HostelManagementSystem_Backend.dto.impl;
+
+
+import com.example.HostelManagementSystem_Backend.dto.SuperDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentResponseDto implements SuperDto {
+
+    private String studentId;
+    private String name;
+    private String nic;
+    private String email;
+    private String tel;
+    private String address;
+    private LocalDate dob;
+    private String specialThings;
+    private String jobEducationInstitute;
+    private String positionFaculty;
+    private String batch;
+    private String degree;
+    private String parentId;
+    private String parentName;
+}
