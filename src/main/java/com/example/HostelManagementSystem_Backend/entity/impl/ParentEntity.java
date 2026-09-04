6e4c2d3013fed94fac8733d3440a53ce8e539882
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-04 23:27:49 +0530

    remove unnecessary import statements in ParentEntity

--- src/main/java/com/example/HostelManagementSystem_Backend/entity/impl/ParentEntity.java
+++ src/main/java/com/example/HostelManagementSystem_Backend/entity/impl/ParentEntity.java
@@ -5,8 +5,6 @@ import com.example.HostelManagementSystem_Backend.entity.SuperEntity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
-import java.time.LocalDate;
 import java.util.List;
 @Data
 @Entity
