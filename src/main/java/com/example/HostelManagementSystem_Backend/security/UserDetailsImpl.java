6bc09ddead7acf295a86a17d53cb2cb518f37546
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-10 02:35:36 +0530

    UserDetailsServiceImpl to load user-specific data from database for Spring Security authentication

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/security/UserDetailsImpl.java
@@ -0,0 +1,53 @@
+package com.example.HostelManagementSystem_Backend.security;
+
+import com.example.HostelManagementSystem_Backend.entity.impl.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private final UserEntity user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
