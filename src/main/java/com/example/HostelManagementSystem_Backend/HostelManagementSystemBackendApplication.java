7f58680ed3ef6ca762c196420eb2b43161597def
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-10 02:40:27 +0530

    integrate Dotenv configuration into main application class

--- src/main/java/com/example/HostelManagementSystem_Backend/HostelManagementSystemBackendApplication.java
+++ src/main/java/com/example/HostelManagementSystem_Backend/HostelManagementSystemBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.HostelManagementSystem_Backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HostelManagementSystemBackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(HostelManagementSystemBackendApplication.class, args);
 	}
 
