8c69c837dca3182a76e15430e59d4638d2cb03db
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-09-10 02:21:47 +0530

    Create JpaRepository UserRepository add methods to findByUsername and exist by username

---
+++ src/main/java/com/example/HostelManagementSystem_Backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.HostelManagementSystem_Backend.repository;
+
+
+import com.example.HostelManagementSystem_Backend.entity.impl.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
